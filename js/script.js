0ce2bc3c947ae6194ec063415be19b6efb1ed7e7
burillu <burillu90@hotmail.it>
2023-10-31 15:28:36 +0100

    add todolist, html header,  main, milestone 1 ok

--- js/script.js
+++ js/script.js
@@ -5,7 +5,30 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        toDoList:[
+          {
+            text:'Fare la Spesa',
+            done:true,
+            id:1
+          },
+          {
+            text:'Fare la lavatrice',
+            done:false,
+            id:2
+          },
+          {
+            text:'Pagare l\' affitto',
+            done:false,
+            id:3
+          },
+          
+        ],
+        lastId:3,
+
       }
+    },
+    methods:{
+      
     }
   }).mount('#app')
